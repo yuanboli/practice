da15e725312ea7540dba6fe4995e18d9ceb32434
yuanboli <yboli@outlook.com>
2017-02-17 13:36:42 -0800

    basic draft 878

---
+++ onlinejudge/UVa/dynamic_programming/878_Max_Subsequence_Product.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.*
+
+class Solution
+{
+	public static int main()
+	{
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext())
+		{
+			BigInteger max = new BigInteger();
+			BigInteger min = new BigInteger();
+
+			int cur;
+			while()
+		}
+	}
+}
