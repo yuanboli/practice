3bf8bf3b45c0c06ad357bf767b6016f59b4ac691
yuanboli <yboli@outlook.com>
2017-03-16 13:04:20 -0700

    need debug

--- onlinejudge/Poj/DP/poj3071.java
+++ onlinejudge/Poj/DP/poj3071.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Main{
-	static void main(String argv[]){
+	public static void main(String argv[]){
 		Scanner scan = new Scanner(System.in);
 		int n;
 		n = scan.nextInt();
@@ -10,13 +10,15 @@ class Main{
 			/*content*/
 			
 			//scan table p
-			double[][] p = new double[n+1][n+1];
-			for(int i = 1; i < n+1; i++){
-				for(int j = 1; j < n+1; j++){
+			double[][] p = new double[(1<<n)+1][(1<<n)+1];
+			for(int i = 1; i < (1<<n)+1; i++){
+				for(int j = 1; j < (1<<n)+1; j++){
 					p[i][j] = scan.nextDouble();
 				}
 				scan.nextLine();
 			}
+
+			printMatrix(p, n, n);
 			
 			//calculate dp
 			double[][] dp = new double[n+1][(1 << n) + 1];
@@ -30,7 +32,7 @@ class Main{
 						dp[i][j] = dp[i-1][j] * sum;
 					}else{
 						double sum = 0;
-						for(int k = 1; k < 1 + (1<<(n-1); k++){
+						for(int k = 1; k < 1 + (1<<(n-1)); k++){
 							sum += p[i-1][k];
 						}
 						dp[i][j] = dp[i-1][j] * sum;
@@ -51,5 +53,16 @@ class Main{
 			n = scan.nextInt();
 			scan.nextLine();
 		}
+	}
+
+
+	public static void printMatrix(double[][] matrix, int rowNum, int colNum){
+		for(int i = 0; i < rowNum; i++){
+			for(int j = 0; j < colNum; j++){
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	
 	}
 }
