3b2cf231f1ff91da9e5a633834976c67228b442d
Yuanbo Li <yboli@outlook.com>
2017-03-29 10:25:36 -0700

    small modification

--- onlinejudge/Poj/DP/poj3071.java
+++ onlinejudge/Poj/DP/poj3071.java
@@ -17,15 +17,58 @@ class Main{
 				}
 				scan.nextLine();
 			}
+			
+			
+			
+			double[][] dp = new double[n+1][(1 << n) + 1];
+			
+			for(int i = 0; i < (1<<n)+1; i++){
+				p[i][0] = 1;
+				if(i < n+1)
+					dp[i][0] = 1;
+				p[0][i] = 1;
+				dp[0][i] = 1;
+			}
 
-			printMatrix(p, n, n);
 			
 			//calculate dp
-			double[][] dp = new double[n+1][(1 << n) + 1];
 			for(int i = 1; i < n + 1; i++){
 				for(int j = 1; j < (1 << n) + 1; j++){
+					if(i == 1){
+						if(j % 2 == 1){	//first team
+							dp[i][j] = p[j][j+1];
+						}else{ //second team
+							dp[i][j] = p[j][j-1];
+						}
+					}
+					else{
+						/*calculate sum*/
+						int sec_num = (j - 1) / (1 << i);
+						int sec_len = 1 << i;
+						int position = (j - 1) % (1 << i);
+						double sum = 0;
+						if(position + 1 <= (1 << (i-1))){
+							//in the first half
+							for(int index = 1; index < (1 << (i-1))+1; index++){
+								int secondTeam = sec_num * sec_len + index + sec_len/2;
+								sum += dp[i-1][secondTeam] * p[j][secondTeam];
+							}
+						}else{
+							//in the second half
+							for(int index = 1; index < (1 << (i-1))+1; index++){
+								int secondTeam = sec_num * sec_len + index;
+								sum += dp[i-1][secondTeam] * p[j][secondTeam];
+							}
+						}
+						
+						
+						/*calculate dp*/
+						dp[i][j] = dp[i-1][j] * sum;
+					}
+					/*
 					if(j <= (1 << (n-1))){
 						double sum = 0;
+						
 						for(int k = 1 + (1<<(n-1)); k < 1 + (1<<n); k++){
 							sum += p[i-1][k];
 						}
@@ -36,15 +79,20 @@ class Main{
 							sum += p[i-1][k];
 						}
 						dp[i][j] = dp[i-1][j] * sum;
-					}
+					}*/
 				}
 			}
 			
+//			printMatrix(dp, 1+n, 1+(1<<n));
+			
+			
 			//find solution
-			int maxIndex = 0, maxValue = 0;
+			int maxIndex = 0; 
+			double maxValue = 0;
 			for(int i = 1; i < (1 << n) + 1; i++){
-				if(p[n][i] > maxValue){
+				if(dp[n][i] > maxValue){
 					maxIndex = i;
+					maxValue = dp[n][i];
 				}
 			}
 			System.out.println(maxIndex);
@@ -55,7 +103,7 @@ class Main{
 		}
 	}
 
-
+/*
 	public static void printMatrix(double[][] matrix, int rowNum, int colNum){
 		for(int i = 0; i < rowNum; i++){
 			for(int j = 0; j < colNum; j++){
@@ -63,6 +111,6 @@ class Main{
 			}
 			System.out.println();
 		}
-	
 	}
+*/
 }
