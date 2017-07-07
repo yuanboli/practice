8248bcaa88dff6be55fb5b24b3bef14002f4b7bc
Yuanbo Li <yboli@outlook.com>
2017-07-07 11:25:52 -0700

    need to figure out why int does not work

--- onlinejudge/ucd_acm_team/7-6/happy_camper.cpp
+++ onlinejudge/ucd_acm_team/7-6/happy_camper.cpp
@@ -2,18 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int l; //limit
-int p; //period
-int v; //vacation
+long long l; //limit
+long long p; //period
+long long v; //vacation
 int main(){
     int caseNum = 1;
     while(cin >> l >> p >> v){
         if(l == 0 && p == 0 && v == 0){
             break;
         }
-        int t = v / p;
-        int result = t * l + min(l, v % p);
+        long long t = v / p;
+        long result = t * l + min(l, v % p);
         cout << "Case " << caseNum << ": " << result << endl;
         caseNum ++;
     }
+    return 0;
 }
