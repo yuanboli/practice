43ae764f31d5b0ec0187aac6324bc001a99c9e57
yuanboli <yboli@outlook.com>
2017-07-09 19:07:24 -0700

    draft finish

--- onlinejudge/ucd_acm_team/7-6/chemistry.cpp
+++ onlinejudge/ucd_acm_team/7-6/chemistry.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 //will assume itr will not be line.end()
-string nextToken(string::iterator itr, string str){
+string nextToken(string::iterator& itr, string& str){
     string result;
     if(isupper(*itr)){
         //a new element
-        result = to_string(*itr);
+        result = string(1, *itr);
         itr++;
         while(itr != str.end() && islower(*itr)){
-            result += to_string(*itr);
+            result += string(1, *itr);
             itr++;
         }
     }else if(*itr == '('){
@@ -22,10 +22,10 @@ string nextToken(string::iterator itr, string str){
         itr++;
         return ")";
     }else if(isdigit(*itr)){
-        result = to_string(*itr);
+        result = string(1, *itr);
         itr++;
         while(itr != str.end() && isdigit(*itr)){
-            result += to_string(*itr);
+            result += string(1, *itr);
             itr++;
         }
     }
@@ -36,45 +36,113 @@ string nextToken(string::iterator itr, string str){
 
 class Env{
 public:
-    map<string, int> *atomCounter;
+    map<string, long long> *atomCounter;
     Env(){
-        atomCounter = new map<string, int>();
+        atomCounter = new map<string, long long>();
     }
     ~Env(){
         free(atomCounter);
     }
+
+	void mul(long long n){
+		for(map<string, long long>::iterator itr = atomCounter->begin(); itr != atomCounter->end(); itr++){
+			itr->second = itr->second * n;
+		}
+	}
+
+	void add(Env* rhs){
+		for(map<string, long long>::iterator itr = rhs->atomCounter->begin(); itr != rhs->atomCounter->end(); itr++){
+			map<string, long long>::iterator targetItr = atomCounter->find(itr->first);
+			if(targetItr != atomCounter->end()){
+				targetItr->second += itr->second;
+			}else{
+				atomCounter->insert(make_pair(itr->first, itr->second));
+			}
+		}
+	}
 };
 
 //get the iterator of the string, return the newly allocated Env
-Env* getOneLevelEnv(string::iterator itr, string str){
+Env* getOneLevelEnv(string::iterator& itr, string& str){
     string s("");
-    pair<string, int> p = make_pair(s, 1);
+    pair<string, long long> p = make_pair(s, 1);
     Env *e = new Env();
-    for(itr; itr != str.end(); itr++){
+	Env *temp = NULL;
+    while(itr != str.end()){
         string token = nextToken(itr, str);
         if(isupper(token[0])){
             //token represents an atom
-            if(p.first != ""){
-                e->atomCounter->insert(p);
+			if(temp != NULL){
+				e->add(temp);
+				free(temp);
+				temp = NULL;
+			}else if(p.first != ""){
+				map<string, long long>::iterator tempItr = e->atomCounter->find(p.first);
+				if(tempItr != e->atomCounter->end()){
+					tempItr->second += p.second;
+				}else{
+                	e->atomCounter->insert(p);
+				}
+				//p = make_pair("", 1);
             }
             p = make_pair(token, 1);
-        }
+        }else if(isdigit(token[0])){
+			if(temp != NULL){
+				//precede it is a block
+				temp->mul(stoll(token));
+			}else{
+				p.second = stoll(token);
+			}
+		}else if(token == "("){
+			if(temp != NULL){
+				e->add(temp);
+				free(temp);
+				temp = NULL;
+			}else if(p.first != ""){
+				map<string, long long>::iterator tempItr = e->atomCounter->find(p.first);
+				if(tempItr != e->atomCounter->end()){
+					tempItr->second += p.second;
+				}else{
+                	e->atomCounter->insert(p);
+				}
+				p = make_pair("", 1);
+			}
+			temp = getOneLevelEnv(itr, str);
+		}else{
+			//token == ")"
+			//itr++;
+			break;
+		}
     }
+	if(temp != NULL){
+		e->add(temp);
+		free(e);
+		temp = NULL;
+	}else if(p.first != ""){
+		map<string, long long>::iterator tempItr = e->atomCounter->find(p.first);
+		if(tempItr != e->atomCounter->end()){
+			tempItr->second += p.second;
+		}else{
+        	e->atomCounter->insert(p);
+		}
+		p = make_pair("", 1);
+	}
+	
     return e;
 }
 
 string line = string();
 int main(){
     while(getline(cin, line)){
-        Env* e = getOneLevelEnv(line.begin(), line);
-        map<string, int>::iterator itr = e->atomCounter->begin();
+		string::iterator itr_str = line.begin();
+        Env* e = getOneLevelEnv(itr_str, line);
+        map<string, long long>::iterator itr = e->atomCounter->begin();
         cout << itr->second << itr->first;
         itr++;
         for(itr; itr != e->atomCounter->end(); itr++){
             cout << "+" << itr->second << itr->first;
         }
         cout << endl;
-        getchar();
         free(e);
     }
 }
