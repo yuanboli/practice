652e08698b2662e7f7d12b3f6f57dc3ed530bbb9
yuanboli <yboli@outlook.com>
2017-11-04 19:48:38 -0700

    try to build a lexer, now lacking add distinguish tokens

---
+++ compiler_practice/llvm-3.8/Transforms/LLVMLexer/LLVMLexer.cpp
@@ -0,0 +1,29 @@
+#include "llvm/ADT/Statistic.h"
+#include "AsmWriter.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+
+#define DEBUG_TYPE "hello"
+
+STATISTIC(HelloCounter, "Counts number of functions greeted");
+
+namespace {
+  // Hello - The first implementation, without getAnalysisUsage.
+  struct LLVMLexer : public ModulePass {
+    static char ID; // Pass identification, replacement for typeid
+    LLVMLexer() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M) override {
+      ++HelloCounter;
+      M.print(errs(), nullptr, false, false);
+      return false;
+    }
+  };
+}
+
+char LLVMLexer::ID = 0;
+static RegisterPass<LLVMLexer> X("llvm-lexer", "LLVM Lexer Pass: print tokens of IR");
+
